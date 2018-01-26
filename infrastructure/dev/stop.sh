767868fd41304fa8a504311bbb09f3f8e9bc87f9
gmicsa <georgian.micsa@gmail.com>
2018-01-26 22:10:54 +0200

    add bash scripts for running locally with docker-compose; add volume for mongodb

---
+++ infrastructure/dev/stop.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker-compose down
+docker volume prune -f
--- src/main/java/ro/micsa/resource/ScoresResource.java
+++ src/main/java/ro/micsa/resource/ScoresResource.java
@@ -3,6 +3,8 @@ package ro.micsa.resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ro.micsa.domain.Score;
@@ -12,6 +14,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/scores")
+@Slf4j
 public class ScoresResource {
 
     @Autowired
@@ -20,12 +23,16 @@ public class ScoresResource {
     @GetMapping
     @ResponseBody
     public Flux<Score> getScores(@RequestParam(required = false) String team) {
+        log.info("Get scores" + (team != null ? " for team " + team : ""));
+
         return StringUtils.isEmpty(team) ? scoresService.getScores() : scoresService.getScoresForTeam(team);
     }
 
     @PostMapping
     @ResponseBody
     public Mono<Score> addScore(@RequestBody @Valid Score score) {
+        log.info("Add score " + score);
+
         return scoresService.addScore(score);
     }
 
